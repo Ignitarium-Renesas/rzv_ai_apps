42ac902f62e9994a3df9baba8d38461fc1de2ee4
lolikapadmanabhan <lolika.padmanabhan@ignitarium.com>
2024-01-30 14:38:23 +0530

    updated wayland code for display

---
+++ 11_Head_count_topview/src/build/CMakeFiles/head_count_topview_app.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/11_Head_count_topview/src/MeraDrpRuntimeWrapper.cpp" "/home/11_Head_count_topview/src/build/CMakeFiles/head_count_topview_app.dir/MeraDrpRuntimeWrapper.cpp.o"
+  "/home/11_Head_count_topview/src/box.cpp" "/home/11_Head_count_topview/src/build/CMakeFiles/head_count_topview_app.dir/box.cpp.o"
+  "/home/11_Head_count_topview/src/head_count_topview.cpp" "/home/11_Head_count_topview/src/build/CMakeFiles/head_count_topview_app.dir/head_count_topview.cpp.o"
+  "/home/11_Head_count_topview/src/wayland.cpp" "/home/11_Head_count_topview/src/build/CMakeFiles/head_count_topview_app.dir/wayland.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMAGE_MODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/drp-tvm_dev-v2.1.0_231227/tvm/include"
+  "/home/drp-tvm_dev-v2.1.0_231227/tvm/3rdparty/dlpack/include"
+  "/home/drp-tvm_dev-v2.1.0_231227/tvm/3rdparty/dmlc-core/include"
+  "/home/drp-tvm_dev-v2.1.0_231227/tvm/3rdparty/compiler-rt"
+  "/opt/poky/3.1.21/sysroots/aarch64-poky-linux/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
