ad00b991f95e48119c937a049148a296b6ebe44c
lolikapadmanabhan <lolika.padmanabhan@ignitarium.com>
2024-02-12 14:38:43 +0530

    updated opencv to wayland+GPU display and added seperate object files for VGA and FHD

---
+++ 02_Line_crossing_object_counting/src/build/CMakeFiles/line_crossing_app.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/Line_crossing.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/Line_crossing.cpp.o"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/MeraDrpRuntimeWrapper.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/MeraDrpRuntimeWrapper.cpp.o"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/box.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/box.cpp.o"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/kalman_filter.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/kalman_filter.cpp.o"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/matrix.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/matrix.cpp.o"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/munkres.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/munkres.cpp.o"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/track.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/track.cpp.o"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/tracker.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/tracker.cpp.o"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/wayland.cpp" "/home/sharady/ai_sdk_work/drp-ai_tvm/02_Line_crossing_yolov3_wayland_02_feb_working/02_Line_crossing_yolov3_wayland_02_feb/02_Line_crossing/src/build/CMakeFiles/line_crossing_app.dir/wayland.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMAGE_MODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/tvm/include"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/tvm/3rdparty/dlpack/include"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/tvm/3rdparty/dmlc-core/include"
+  "/home/sharady/ai_sdk_work/drp-ai_tvm/tvm/3rdparty/compiler-rt"
+  "/opt/poky/3.1.21/sysroots/aarch64-poky-linux/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
