c447fea19af85e35ee736f89fc586fb9255ca6ea
lolikapadmanabhan <lolika.padmanabhan@ignitarium.com>
2024-02-13 17:18:22 +0530

    updated wayland+GPU for display

---
+++ 07_Animal_Detection/src/build/CMakeFiles/3.16.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.15.0-94-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.15.0-94-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/sharady/ai_sdk_work/drp-ai_tvm/renesas_rzv2l_drp-ai-tvm/07_Animal_Detection_vga/src/toolchain/runtime.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
