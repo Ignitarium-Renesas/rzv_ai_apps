b9030db29b0e107f2bcd7e7b0c25562443fbbcd0
lolikapadmanabhan <lolika.padmanabhan@ignitarium.com>
2024-05-03 21:03:08 +0530

    updated the changes with respect to DRP-AI TVM v2.2.1

--- 07_Animal_detection/src/animal_detection.cpp
+++ 07_Animal_detection/src/animal_detection.cpp
@@ -54,10 +54,10 @@ static Wayland wayland;
 static pthread_t ai_inf_thread;
 static pthread_t kbhit_thread;
 static sem_t terminate_req_sem;
-static int32_t drp_max_freq;
-static int32_t drp_freq;
+
 
 static uint32_t disp_time = 0;
+static int32_t drpai_freq;
 
 std::string media_port;
 std::string gstreamer_pipeline;
@@ -443,7 +443,7 @@ int Animal_Detection()
 
     /* Inference time start */
     auto t2 = std::chrono::high_resolution_clock::now();
-    runtime.Run();
+    runtime.Run(drpai_freq);
     /* Inference time end */
     auto t3 = std::chrono::high_resolution_clock::now();
     auto inf_duration = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count();
@@ -703,39 +703,6 @@ uint64_t get_drpai_start_addr(int drpai_fd)
     return drpai_data.address;
 }
 
-/*****************************************
-* Function Name : set_drpai_freq
-* Description   : Function to set the DRP and DRP-AI frequency.
-* Arguments     : drpai_fd: DRP-AI file descriptor
-* Return value  : 0 if succeeded
-*                 not 0 otherwise
-******************************************/
-int set_drpai_freq(int drpai_fd)
-{
-    int ret = 0;
-    uint32_t data;
-
-    errno = 0;
-    data = drp_max_freq;
-    ret = ioctl(drpai_fd , DRPAI_SET_DRP_MAX_FREQ, &data);
-    if (-1 == ret)
-    {
-        std::cerr << "[ERROR] Failed to set DRP Max Frequency : errno=" << errno << std::endl;
-        return -1;
-    }
-
-    errno = 0;
-    data = drp_freq;
-    ret = ioctl(drpai_fd , DRPAI_SET_DRPAI_FREQ, &data);
-    if (-1 == ret)
-    {
-        std::cerr << "[ERROR] Failed to set DRP-AI Frequency : errno=" << errno << std::endl;
-        return -1;
-    }
-
-    return 0;
-}
-
 
 /*****************************************
 * Function Name : init_drpai
@@ -752,18 +719,12 @@ uint64_t init_drpai(int drpai_fd)
 
     /*Get DRP-AI memory start address*/
     drpai_addr = get_drpai_start_addr(drpai_fd);
+
+
     if (drpai_addr == 0)
     {
         return 0;
     }
-
-    /*Set DRP-AI frequency*/
-    ret = set_drpai_freq(drpai_fd);
-    if (ret != 0)
-    {
-        return 0;
-    }
-
 
     return drpai_addr;
 }
@@ -960,29 +921,30 @@ int main(int argc, char *argv[])
     {   
         if (argc >= 3 )
         {
-            drp_max_freq = atoi(argv[2]);
+            drpai_freq = atoi(argv[2]);
+            if ((1 <= drpai_freq) && (127 >= drpai_freq))
+            {
+                printf("Argument : <AI-MAC_freq_factor> = %d\n", drpai_freq);
+            }
+            else
+            {
+                fprintf(stderr,"[ERROR] Invalid Command Line Argument : <AI-MAC_freq_factor>=%d\n", drpai_freq);
+                return -1;
+            }
+        
         }
         else
         {
-            drp_max_freq = DRP_MAX_FREQ;
-        }
-        if (argc >= 4)
-        {
-            drp_freq = atoi(argv[3]);
-        }
-        else
-        {
-            drp_freq = DRPAI_FREQ;
+            drpai_freq = DRPAI_FREQ;
         }
     }
-        
     else
     {
-        std::cout<<"Support for USB mode only"<<std::endl;
+        std::cout<<"Support for USB mode only."<<std::endl;
         return -1;
     }
 
-    if (argc>6)
+    if (argc>3)
     {
         std::cerr << "[ERROR] Wrong number Arguments are passed " << std::endl;
         return 1;
